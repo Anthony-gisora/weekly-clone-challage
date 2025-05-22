41180e49452d107d2871cbf00a9648a6daa2b8e4
Tonny_G <gesoranthony@gmail.com>
2025-05-22 18:07:00 +0300

    Updated my abouts

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -17,22 +17,25 @@ const AboutMe = () => {
       <div className="w-full md:w-1/2 md:pl-12 text-center md:text-left">
         <h2 className="text-3xl font-bold mb-4">👋 Hey, I'm Tonny!</h2>
         <p className="text-lg mb-4">
-          I'm a versatile full-stack developer with a strong focus on modern web
-          and mobile technologies. I specialize in building smooth, responsive
-          UIs and powerful backend systems.
+          I'm a full-stack developer with a strong focus on modern web and
+          mobile technologies. I specialize in building smooth, responsive UIs
+          and backend APIs, gateways for web apps and android applications.
         </p>
         <p className="text-lg mb-4">
-          On the frontend, I work with <strong>React</strong>,{" "}
+          On the frontend, I work with <strong>React</strong>,
           <strong>Next.js</strong>, and <strong>React Native</strong> to create
-          clean and dynamic interfaces. For backend and data, I integrate{" "}
-          <strong>Firebase</strong>, <strong>MongoDB</strong>, and{" "}
-          <strong>REST APIs</strong> to deliver real-time, scalable solutions.
+          clean and dynamic interfaces. For backend and data, I integrate
+          <strong>Firebase</strong>, <strong>MongoDB</strong>, and
+          <strong>REST APIs</strong> to deliver real-time solutions.
         </p>
         <p className="text-lg mb-4">
-          I'm also experienced with{" "}
-          <strong>authentication systems like Clerk</strong>, deployment, and
-          connecting complex workflows to create full products — from admin
-          dashboards to mobile apps.
+          I'm also experienced with
+          <strong>
+            authentication systems like Clerk, Next auth, google auth...
+          </strong>{" "}
+          and flexible to integrate an authentication service of your choice,
+          deployment, and connecting web apps to create full products — from
+          admin dashboards to mobile apps.
         </p>
         <p className="text-lg">
           Always learning, always building. Let’s ship something impactful
